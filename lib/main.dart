3cddc637a1986ff51380d7bf63a2f1575e8f84d9
Laxman <aaa@Laxmans-MacBook.local>
2025-12-13 23:50:49 +0545

    initialized firebase in our main project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:kurakani/auth/login_or_register.dart';
-
+import 'package:kurakani/auth/auth_gate.dart';
+import 'package:kurakani/firebase_options.dart';
 import 'package:kurakani/theme/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -15,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightMode,
-      home: LoginOrRegister(),
+      home: AuthGate(),
     );
   }
 }
